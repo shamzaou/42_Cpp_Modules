9b0342dd07b91ac4a4e1e5749c34b3a6f0c8926f
shamzaou <shamzaou@student.42abudhabi.ae>
2024-01-24 15:02:56 +0400

    update

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shamzaou <shamzaou@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 11:41:10 by shamzaou          #+#    #+#             */
-/*   Updated: 2024/01/24 11:52:12 by shamzaou         ###   ########.fr       */
+/*   Updated: 2024/01/24 13:48:24 by shamzaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int main()
 {
+    std::cout << "Stack allocated:" << std::endl;
     Zombie zombie1("Karen");
     zombie1.announce();
 
+    std::cout << "\nHeap allocated:" << std::endl;
     Zombie* zombie2 = newZombie("Steve");
     zombie2->announce();
     delete zombie2;
 
+    std::cout << "\nRandom Chump:" << std::endl;
     randomChump("Bob");
 
     return 0;
